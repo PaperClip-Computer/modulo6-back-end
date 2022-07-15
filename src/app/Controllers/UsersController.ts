d485fad3f658111f74f98cbdde3f597417fbdbaa
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2022-07-15 00:49:01 -0300

    WIP: routes and controllers

---
+++ src/app/Controllers/UsersController.ts
@@ -0,0 +1,24 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default class UsersController {
+  public async list({ request, response }: HttpContextContract) {
+    return response.json(null)
+  }
+  public async get({ request, response }: HttpContextContract) {
+    return response.json(null)
+  }
+
+  public async post({ request, response }: HttpContextContract) {
+    return response.json(null)
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    return response.json(null)
+  }
+  public async delete({ request, response }: HttpContextContract) {
+    return response.json(null)
+  }
+}
---
+++ src/app/Validators/StoreBloodType.ts
@@ -0,0 +1,8 @@
+export default class StoreBloodType {
+  get rules() {
+    return {
+      rh: 'required',
+      type: 'required',
+    }
+  }
+}
